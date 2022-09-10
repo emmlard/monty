3aba109783e0bd4a3b3d06e2e9a4e63f71d65923
emmlard <emmlard@gmail.com>
2022-09-10 12:17:24 +0100

    initial commit

---
+++ nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * _nop - The opcode nop does nothing
+ * @head: double pointer to header (top) of the stack
+ * @line_number: counter for line number of the file
+ * Author: Excel Nwachukwu
+ *
+ * Return: void
+ */
+void _nop(stack_t **head, unsigned int line_number)
+{
+	(void) head;
+	(void) line_number;
+}
+
